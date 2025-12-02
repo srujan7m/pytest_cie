eb1228ec26c7246600b1a66ba90bd180f161d558
Srujan Mattur <srujanmmattur@gmail.com>
2025-12-02 11:56:45 +0530

    Update test_vehicle.py

--- test_vehicle.py
+++ test_vehicle.py
@@ -1,7 +1,8 @@
 from vehicle import vehicle_details
+
 def test_vehicle_details():
     expected_output = (
-        "Vehicle Number:KA27EM1234\n"
+        "Vehicle Number: KA27EM1234\n"
         "Owner name: Srujan Mattur\n"
         "Vehicle Type: Bike\n"
         "Year of Manufacture: 2018"
